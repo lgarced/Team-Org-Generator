1906af6cc50a580844eda6018e5bcda423c50666
Liberato Garced <93692943+lgarced@users.noreply.github.com>
2022-05-28 10:05:40 -0700

    Adding class

--- index.js
+++ index.js
@@ -1,23 +1,91 @@
 //10 Challenge
-class Employee {
-  constructor(name, email) {
-    (this.name = name),
-      (this.email = email),
-      (this.manager = "Libearati"),
-      (this.manageremail = "lberato@garced.dev");
-  }
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+const employeeArr = [];
+
+
+const questions = () => {
+  prompt([
+    {
+      message: "What type of employee would you like to create?",
+      type: "list",
+      name: "type",
+      choices: ["Engineer", "Manager", "Intern"],
+    },
+    {
+      type: "input",
+      name: "nameInput",
+      message: "What is the employee's name?",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter a name");
+          return false;
+        }
+      },
+    },
+    {
+      message: "What is the employee's id?",
+      type: "input",
+      name: "idInput",
+      validate: (idInput) => {
+        if (idInput) {
+          return true;
+        } else {
+          console.log("You must enter an ID to continue");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "emailInput",
+      message: "What is employee's email?",
+      validate: (emailInput) => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log("You must enter an email to continue");
+          return false;
+        }
+      },
+    },
+  ]).then((emp) => {
+    const extraQ = additionalQuestions[emp.type];
+
+    prompt(extraQ).then(({ extra }) => {
+      console.log("extra value --- ", extra);
+
+      const newEmp = new Engineer(emp.name, emp.id, emp.email, extra);
+      employees.push(newEmp);
+
+      console.log(`${emp.type} Created!`);
+      setTimeout(start, 1500);
+    });
+  });
+};
+
+// arr.map((currentItem, i) => {
+//   employeeArr[i] = new Employee(currentItem.name, currentItem.email);
+// });
+
+
+inquirer
+  .prompt(questions)
+  .then(function (response) {
+    let answers = generateMarkdown(response);
+    console.log(answers);
+    writeDocument(response.document, answers);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+function writeDocument(filename, answer) {
+  fs.writeFileSync(path.join(`${filename}.html`), answer);
 }
 
-const companyEmployees = new Employee();
-
-const arr = [
-  { name: "Liberato", email: "liberato@garced.dev", coding: true },
-  { name: "Erik", email: "erik@garced.dev", coding: true },
-  { name: "Marcos", email: "marcos@garced.dev", coding: false },
-];
-const employeeArr = [];
-arr.map((currentItem, i) => {
-  employeeArr[i] = new Employee(currentItem.name, currentItem.email);
-});
 
 console.log(employeeArr);
